631740891835d7bb920580ac2bc30810423627dd
ArjunKunwar <arjunkunwar16aka@gmail.com>
2025-03-11 17:57:39 +0530

    Add getUserChannelProfile endpoint to retrieve channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadToCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import { Subscription } from "../models/subscription.model.js";
 
 const generateAccessAndRefreshToken = async (userId) => {
     try{
@@ -319,6 +320,72 @@ const updateUserCover = asyncHandler(async(req, res) => {
     
 })
 
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+
+    const {username} = req.params;
+
+    if(!username){
+        throw new ApiError(400, "Please provide username");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username
+            }
+        },
+        {
+            $lookup: {
+                from : "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as : "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                SubscriberCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $in: [req.user._id, "$subscribers.subscriber"],
+                    then: true,
+                    else: false
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                avatar: 1,
+                cover: 1,
+                SubscriberCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, channel[0], "Channel found successfully"));
+    
+})
+
 export { registerUser 
         , loginUser
         , logoutUser
@@ -328,6 +395,7 @@ export { registerUser
         , updateAccountDetails
         , updateUserAvatar
         , updateUserCover
+        , getUserChannelProfile
 };      
 
 // import { asyncHandler } from "../utils/asyncHandler.js";
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,44 @@
-import { Router } from 'express';
-import { upload } from '../middlewares/multer.middleware.js';
-import { loginUser, logoutUser, registerUser , refreshAccessToken } from '../controllers/user.controller.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  getCurrentUser,
+  changeCurrentPassword,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCover,
+  getUserChannelProfile,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        { name: "avatar", maxCount: 1 },
-        { name: "cover", maxCount: 1 }
-    ]),
-    registerUser
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  registerUser
 );
 
 router.route("/login").post(loginUser);
 
 //secured routes
 
-router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/update-account").put(verifyJWT, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .put(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover")
+  .put(verifyJWT, upload.single("cover"), updateUserCover);
+router.route("/channel/:username").get(getUserChannelProfile);
 
 export default router;
